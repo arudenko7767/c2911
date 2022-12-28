cd233fa7156f53c219e50fcdacf7635f11498f2d
arudenko7767 <rudenkoaleksej996@gmail.com>
2022-12-28 16:59:34 +0300

    initial commit

---
+++ pythonProject/pythonProject1/main.py
@@ -0,0 +1,22 @@
+#class Student:
+    #def __init__(self):
+        #self.height=170
+#nick=Student()
+#print(nick.height)
+
+#class Rec:
+    #def area(self,a , b):
+        #return a * b
+#rect=Rec()
+#print(rect.area(4,5))
+
+class Me:
+    a=1
+    b=3
+    c=2007
+day=Me
+months=Me
+year=Me
+print(day.a)
+print(months.b)
+print(year.c)
